83a9e7644af1d749ba1c433aad55c2ae8a7d7f8a
raul hernandez <raulhernandezing@gmail.com>
2021-03-26 12:06:37 -0600

    rojects

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -54,9 +54,9 @@ class HomeController extends Controller
 
    $donations = Donation::get();
     $division =     DB::select(" 
-    SELECT projects.id, rojects.name, SUM(donations.amount) AS TOTALAMOUNT
+    SELECT projects.id, projects.name, SUM(donations.amount) AS TOTALAMOUNT
     FROM  projects INNER JOIN  donations
-    ON rojects.id=donations.project_id
+    ON projects.id=donations.project_id
     GROUP BY projects.id, projects.name
 ");
    
